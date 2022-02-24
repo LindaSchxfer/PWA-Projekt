91cf8005bcf1c3a8f0bf235f4e17bc5dd5b0976d
melis-sy <74507515+melis-sy@users.noreply.github.com>
2022-02-24 14:58:58 +0100

    Hinzufügen-Button erstellt

--- shop-the-top/src/app/hinzufuegen/hinzufuegen.component.ts
+++ shop-the-top/src/app/hinzufuegen/hinzufuegen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingItem } from '../shopping-item';
+import { ShoppingItemService } from '../shopping-item.service';
 
 @Component({
   selector: 'app-hinzufuegen',
@@ -7,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HinzufuegenComponent implements OnInit {
 
-  constructor() { }
+  shoppingItems: ShoppingItem[] = [];
+
+  constructor(private shoppingItemService: ShoppingItemService) { }
 
   ngOnInit(): void {
-    console.log("Hinzufügen ist da");
+    this.loadTodos();
+  }
+
+  async add(productname: string, quantity: number, place: string, unit: string) {
+    await this.shoppingItemService.add(productname, quantity, place, unit);
+    await this.loadTodos();
+  }
+
+  /*async sync() {
+    await this.shoppingItemService.sync();
+    await this.loadTodos();
+  }
+
+  async toggleDone(todo: Todo) {
+    await this.shoppingItemService.toggleDone(todo);
+    await this.loadTodos();
+  }*/
+
+  async loadTodos() {
+    this.shoppingItems = await this.shoppingItemService.getAll();
   }
 
 }
