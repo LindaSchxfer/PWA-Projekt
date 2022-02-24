d1a9d6ef226c97e219b601c9678c31bb2033a50f
melis-sy <74507515+melis-sy@users.noreply.github.com>
2022-02-24 09:22:55 +0100

    Revert "Datenbankanbindung"
    This reverts commit b08a8fe116887cbfad85931d099cbfe88c0f2cc4.

--- shop-the-top/src/app/app.component.ts
+++ shop-the-top/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { SwPush, SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +6,5 @@ import { SwPush, SwUpdate } from '@angular/service-worker';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-pwa-3.1';
-
-  constructor(swUpdate: SwUpdate, private swPush: SwPush, private httpClient: HttpClient ) {
-    swUpdate.available.subscribe(() => {
-      if (confirm( 'Update verfügbar! Jetzt neuladen?')) {
-        location.reload();
-      }
-    });
-  }
-
-  async subscribe() {
-    const subscription = await this.swPush.requestSubscription({
-      //from localhost:3030
-      serverPublicKey: 'BLI8zF79Z1kCQq72RgzYs0WtQ0ojY3XCqPwmgcNP-8LJIeXRep9sv6h41hErJDewrm3WDbFMPyyPhYO7-ClXabQ'
-    });
-    this.httpClient.post('http://localhost:3030/push', subscription.toJSON()).toPromise();
-  }
+  title = 'shop-the-top';
 }
